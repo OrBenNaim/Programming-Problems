41f63703709a2f67a96cc8dab56861d416d1e8bf
Or Ben Naim <orbennaim123@gmail.com>
2024-10-13 23:43:34 +0300

    .

--- GeeksForGeeks/Array Algorithms/array_algorithms.py
+++ GeeksForGeeks/Array Algorithms/array_algorithms.py
@@ -1,6 +1,32 @@
+# https://www.geeksforgeeks.org/top-10-algorithms-in-interview-questions/#1-array
 
-def max_subArray_sum(arr: int) -> arr:
+from typing import List
 
+def max_subArray_sum(arr: List[int]) -> int:
+    res = arr[0]
+    maxEnding = arr[0]
+
+    for i in range(1, len(arr)):
+        maxEnding = max(maxEnding + arr[i], arr[i])
+        res = max(res, maxEnding)
+    
+    return res
+
+
+def find_missing_number(arr: List[int], N: int) -> int:
+    realSum = sum(arr)  
+    arithmeticSum = (N * (1+N)) // 2
+    return arithmeticSum - realSum
 
 
 if __name__ == "__main__":
+    
+    #---------------------- Test Maximum Subarray Sum problem ---------------------- 
+    array = [2, 3, -8, 7, -1, 2, 3]
+    print(f"\nThe maximum sum from all the subarrays is: {max_subArray_sum(array)}\n")
+
+
+    #---------------------- Test Find the Missing Number problem ---------------------- 
+    array = [1, 2, 3, 5]
+    n = 5
+    print(f"\nThe missing number is: {find_missing_number(array, n)}\n")
--- README.md
+++ README.md
@@ -1,5 +1,6 @@
 Link to "Cracking the Coding Interview" book:
 https://englishonlineclub.com/pdf/Cracking%20the%20Coding%20Interview%20-%20189%20Programming%20Questions%20and%20Solutions%20(6th%20Edition)%20[EnglishOnlineClub.com].pdf
 
+
 Link to GeeksForGeeks "Top 10 Algorithms in Interview Questions":
 https://www.geeksforgeeks.org/top-10-algorithms-in-interview-questions/
